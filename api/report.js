b932949dc62099d48521e8d03f07fd2b822b9034
paul christi <rachrisgamer@gmail.com>
2026-01-31 17:47:04 +0700

    Update report.js

--- api/report.js
+++ api/report.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   const form = formidable({
     multiples: false,
-    maxFileSize: 50 * 1024 * 1024,
+    maxFileSize: 20 * 1024 * 1024, // Kurangi jadi 20MB (Vercel limit)
     keepExtensions: true,
     allowEmptyFiles: false,
     minFileSize: 0,
